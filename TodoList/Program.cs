fc3e0738ad96b4718625b8ad66a58041d8a10d62
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-20 20:55:34 +0300

    Add bootstrap to forms and form validation

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -4,10 +4,7 @@ using TodoList.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TodoDbContext>(options =>
@@ -18,7 +15,6 @@ builder.Services.AddScoped<IRepository, TodoRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
