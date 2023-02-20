d1a548a937fb58ea94b0463ae368ef65c97d999a
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-20 11:38:08 +0300

    Add repository to service container

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TodoList.Data;
+using TodoList.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<TodoDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("TodoDb"));
 });
+builder.Services.AddScoped<IRepository, TodoRepository>();
 
 var app = builder.Build();
 
