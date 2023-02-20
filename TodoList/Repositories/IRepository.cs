41d2c8af43225285d88976fd422238a338716759
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-20 12:08:35 +0300

    Update method signature Add method to check the existence of entity

--- TodoList/Repositories/IRepository.cs
+++ TodoList/Repositories/IRepository.cs
@@ -6,8 +6,9 @@ public interface IRepository
 {
     Task AddItem(TodoItem todoItem);
     Task RemoveItem(int id);
-    IEnumerable<TodoItem?> GetItems();
+    Task<IEnumerable<TodoItem>?> GetItems();
     Task<TodoItem?> GetItemById(int id);
     Task EditItem(int id, TodoItem todoItem);
+    public Task<bool> TodoItemExists(int id);
     Task SaveChanges();
 }
